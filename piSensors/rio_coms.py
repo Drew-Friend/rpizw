b4d3595ddeb726161d3a7649f27468323a9b29b3
Drew <dfriend414@gmail.com>
2020-05-18 18:46:49 -0400

    encoding stuff

--- piSensors/rio_coms.py
+++ piSensors/rio_coms.py
@@ -3,7 +3,7 @@ import serial
 
 standin = False
 print("about to serial")
-rio = serial.Serial('/dev/ttyAMA0', 9600, timeout = 0)
+rio = serial.Serial('/dev/ttyAMA0', 9600, timeout = 0, write_timeout = 0)
 
 def disabled():
     if rio.read(1) is 'D':
@@ -15,4 +15,4 @@ def disabled():
 
 def send_value(value):
     print("Value {}".format(value))
-    rio.write(str(value).encode('ascii'))
+    rio.write(value.encode('utf-8'))
