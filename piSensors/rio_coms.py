16ce0801e934cace43954d8bf9dfecf6d0633754
Drew <dfriend414@gmail.com>
2020-05-18 18:15:08 -0400

    coms debugging

--- piSensors/rio_coms.py
+++ piSensors/rio_coms.py
@@ -5,7 +5,8 @@ standin = False
 rio = serial.Serial('/dev/ttyAMA0', 9600)
 
 def disabled():
-    if rio.read(1) is not None:
+    if rio.read(1) is 'D':
+        print("Disabled")
         return True
     else:
         return False
