c44a8d9dcbcaf31e2878b94557bd312fb1a9e064
Drew <dfriend414@gmail.com>
2020-05-18 00:05:11 -0400

    Basic i2c laser code

---
+++ piSensors/hub.py
@@ -0,0 +1,6 @@
+import rangefinder
+from rangefinder import xshut
+
+
+xshut.insert[3]
+rangefinder.setup
--- piSensors/rangefinder.py
+++ piSensors/rangefinder.py
@@ -1,5 +1,18 @@
+
 import time
 import board
 from digitalio import DigitalInOut
 from adafruit_vl53l0x import VL53L0X
+i2c = board.I2C()
+xshut = []
 
+class Laser:
+    def __init__(self, address)
+    self.address = address
+    
+def setup():
+    for i < len(xshut):
+            for j < len(xshut):
+                DigitalInOut(board.xshut[j]) = False
+    DigitalInOut(board.xshut[i]) = True
+    VL53L0X(i2c).setaddress(i + 0x30)
