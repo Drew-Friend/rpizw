4b19b43ed3bb54acc609773606fe5176d00d0f13
Drew <dfriend414@gmail.com>
2020-05-18 18:26:37 -0400

    set timeout

--- piSensors/rio_coms.py
+++ piSensors/rio_coms.py
@@ -3,10 +3,10 @@ import serial
 
 standin = False
 print("about to serial")
-rio = serial.Serial('/dev/ttyAMA0', 9600)
+rio = serial.Serial('/dev/ttyAMA0', 9600, timeout = 0)
 
 def disabled():
-    if rio.read(1) is 'D':
+    if rio.read() is 'D':
         print("Disabled")
         return True
     else:
