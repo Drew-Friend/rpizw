e2722d3b4f6ad1c24ac0996b3da3461f61b98fad
Drew <dfriend414@gmail.com>
2020-05-18 13:59:39 -0400

    testing hub

---
+++ piSensors/hub.py
@@ -0,0 +1,7 @@
+from piSensors.Laser import laser_base
+xshut = laser_base.xshut
+lasers = laser_base.vl53
+
+xshut.append(DigitalInOut(board.D4))
+laser_base.set_addresses()
+laser_base.detect_range()
