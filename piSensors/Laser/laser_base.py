232eabe6daf26fcce8ffbc902092c075748b1806
Drew <dfriend414@gmail.com>
2020-05-18 16:17:25 -0400

    personalizing methods

--- piSensors/Laser/laser_base.py
+++ piSensors/Laser/laser_base.py
@@ -1,47 +1,46 @@
 """
-Example of how to use the adafruit_vl53l0x library to change the assigned address of
-multiple VL53L0X sensors on the same I2C bus. This example only focuses on 2 VL53L0X
-sensors, but can be modified for more. BE AWARE: a multitude of sensors may require
-more current than the on-board 3V regulator can output (typical current consumption during
-active range readings is about 19 mA per sensor).
+1038 VL53L0X sensor library
 """
 import time
 import board
 from digitalio import DigitalInOut
 from Laser.adafruit_vl53l0x import VL53L0X
 
-# declare the singleton variable for the default I2C bus
-i2c = board.I2C()
-# list of xshut pins
-xshut = []
-# list of lasers
-vl53 = []
+
+i2c = board.I2C()       #setup singleton i2c bus
+xshut = []              #list of xshut pins, apended in hub
+vl53 = []               #list of laser objects, ordered in the order the xshut's are declared
 
 
-
-# changes the addresses of the VL53L0X sensors
 def set_addresses():
-    for power_pin in xshut:
-        # makes each xshut pin an output, set to low
+    """ Set a unique address for each laser with an assigned xshut pin """
+    for power_pin in xshut:                     #set each xshut pin to output and turn off
         power_pin.switch_to_output(value=False)
-        # all VL53L0X sensors are now off
+
     for i, power_pin in enumerate(xshut):
-        power_pin.value = True
-        # instantiate the VL53L0X sensor on the I2C bus & insert it into the "vl53" list
-        vl53.insert(i, VL53L0X(i2c)) 
-        if i < len(xshut)-1:
-            # default address is 0x29
-            vl53[i].set_address(i + 0x30)  # address assigned should NOT be already in use
+        power_pin.value = True                  #turn xshut on for selected pin
+        vl53.insert(i, VL53L0X(i2c))            #add a sensor object, in the same order as the xshut's
+        if i < len(xshut)-1:                    # leave last sensor's address 29, before that, they will be increasing
+            vl53[i].set_address(0x30 + i)
+
 
 def reset_addresses():
-    for i, power_pin in enumerate(xshut):
+    """ set all adresses back to zero """
+    for i, power_pin in enumerate(xshut):   #turns on each laser
         power_pin.value = True
-    vl53[i].set_address(0x29)
+        vl53[i].set_address(0x29)           #set each laser to 29 as it turns on
+
 
 def detect_range(count=5):
-    """ take count=5 samples """
+    """ take 5 samples from each sensor"""
     while count:
         for index, sensor in enumerate(vl53):
             print("Sensor {} Range: {}mm".format(index + 1, sensor.range))
         time.sleep(1.0)
         count -= 1
+
+
+def distance(laserNum):
+    """get current distace of a specific laser"""
+    dist = vl53[laserNum].range
+    return dist
--- piSensors/hub.py
+++ piSensors/hub.py
@@ -5,7 +5,13 @@ import board
 xshut = laser_base.xshut
 lasers = laser_base.vl53
 
+enabled = True
+
 xshut.append(DigitalInOut(board.D4))
 laser_base.set_addresses()
-laser_base.detect_range()
+
+while enabled:
+    print(laser_base.distance(0))
+
+#it'd be cool to throw rainbow led code in here
 laser_base.reset_addresses()
