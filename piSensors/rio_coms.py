796239e872232ec0bb4436d71dbec9d75bd7c512
Drew <dfriend414@gmail.com>
2020-05-18 19:28:54 -0400

    experiment

--- piSensors/rio_coms.py
+++ piSensors/rio_coms.py
@@ -6,8 +6,8 @@ print("about to serial")
 rio = serial.Serial('/dev/ttyAMA0', 9600, timeout = 0, write_timeout = 0)
 
 def disabled():
-    if rio.read(1) is not 'b''':
-        print(rio.read(1))
+    if rio.read() is not 'b''':
+        print((rio.read()).decode('utf-8'))
         return True
     else:
         return False
