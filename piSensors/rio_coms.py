96d01e85cabdb547c117b980763dba85f6701b30
Drew <dfriend414@gmail.com>
2020-05-18 18:12:23 -0400

    coms debugging

--- piSensors/rio_coms.py
+++ piSensors/rio_coms.py
@@ -1,11 +1,8 @@
-import busio
 import board
 import serial
 
 standin = False
-#rio = busio.UART(board.TX, board.RX, baudrate=9600)
 rio = serial.Serial('/dev/ttyAMA0', 9600)
-rio.open()
 
 def disabled():
     if rio.read(1) is not None:
