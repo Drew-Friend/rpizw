c78be3bbdf2ca8b0bc6a3e36d772ad52501c727c
Drew <dfriend414@gmail.com>
2020-05-18 19:21:43 -0400

    debug

--- piSensors/rio_coms.py
+++ piSensors/rio_coms.py
@@ -7,7 +7,7 @@ rio = serial.Serial('/dev/ttyAMA0', 9600, timeout = 0, write_timeout = 0)
 
 def disabled():
     if rio.read(1) is not None:
-        print("Disabled")
+        print(rio.read(1))
         return True
     else:
         return False
