df0b218326e57361a50562dd7265fee7472d1e2b
Drew <dfriend414@gmail.com>
2020-05-18 17:46:24 -0400

    starting coms

--- piSensors/rio_coms.py
+++ piSensors/rio_coms.py
@@ -1,12 +1,16 @@
+import busio
+import board
+
 standin = False
+rio = busio.UART(board.TX, board.RX, baudrate=9600)
 
 def disabled():
-    if standin:
+    if rio.read(1) is not None:
         return True
     else:
         return False
 
 
 def send_value(value):
-    #replace print with a serial method eventually
+    rio.write(value)
     print("Value {}".format(value))
