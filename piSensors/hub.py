163a5f48b9ba028122955bbbd22ba3144262aad5
Drew <dfriend414@gmail.com>
2020-05-18 14:27:59 -0400

    changed path

--- piSensors/hub.py
+++ piSensors/hub.py
@@ -1,7 +1,8 @@
-from piSensors.Laser import laser_base
+from Laser import laser_base
 xshut = laser_base.xshut
 lasers = laser_base.vl53
 
 xshut.append(DigitalInOut(board.D4))
 laser_base.set_addresses()
 laser_base.detect_range()
+laser_base.reset_addresses()
