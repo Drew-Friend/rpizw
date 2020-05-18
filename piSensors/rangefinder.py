ca7bc71eee60491c845a9c3cf303ba3c83ad9eb1
Drew <dfriend414@gmail.com>
2020-05-18 00:24:59 -0400

    debugging

--- piSensors/rangefinder.py
+++ piSensors/rangefinder.py
@@ -7,12 +7,12 @@ i2c = board.I2C()
 xshut = []
 
 class Laser:
-    def __init__(self, address)
-    self.address = address
+    def __init__(self, address):
+        self.address = address
     
 def setup():
-    for i < len(xshut):
-            for j < len(xshut):
-                DigitalInOut(board.xshut[j]) = False
-    DigitalInOut(board.xshut[i]) = True
+    for i in range(len(xshut)):
+            for j in range(len(xshut)):
+                DigitalInOut(board.xshut[j]).switch_to_output(value = False)
+    DigitalInOut(board.xshut[i]).switch_to_output(value = True)
     VL53L0X(i2c).setaddress(i + 0x30)
