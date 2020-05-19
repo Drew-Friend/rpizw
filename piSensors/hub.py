06e24f203fd8feb8cbf41eaaa565373373fdadad
Drew <dfriend414@gmail.com>
2020-05-18 22:14:31 -0400

    intify

--- piSensors/hub.py
+++ piSensors/hub.py
@@ -19,7 +19,7 @@ while enabled:
     else:
         print("sending value")
         for i in range(len(lasers)):
-            rio_coms.send_value(laser_base.distance(i))
+            rio_coms.send_value(int(laser_base.distance(i)))
 
 
 #it'd be cool to throw rainbow led code in here
