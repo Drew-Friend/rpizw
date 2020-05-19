386c1041443e25f654784404435643fc03cbb474
Drew <dfriend414@gmail.com>
2020-05-18 22:02:02 -0400

    im tired

--- piSensors/hub.py
+++ piSensors/hub.py
@@ -16,8 +16,8 @@ while enabled:
         enabled = False
     else:
         print("sending value")
-        for i in enumerate(lasers):
-            rio_coms.send_value(laser_base.distance(int[i]))
+        for i in len(lasers):
+            rio_coms.send_value(laser_base.distance(i))
 
 
 #it'd be cool to throw rainbow led code in here
